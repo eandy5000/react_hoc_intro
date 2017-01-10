06dfa0069ce186c248389246e32a53f6a162bf99
Andy Eccleston <eandy5000@yahoo.com>
2017-01-10 16:27:06 -0600

    authenticate is working

--- src/reducers/authentication.js
+++ src/reducers/authentication.js
@@ -1,12 +1,9 @@
-import {
-  CHANGE_AUTH
-} from '../actions/types';
+import { CHANGE_AUTH } from '../actions/types'
 
 export default function(state = false, action) {
-  switch (action.type) {
-    case CHANGE_AUTH:
-      return action.payload;
-  }
-
-  return state;
+    switch(action.type) {
+        case CHANGE_AUTH:
+        return action.payload
+    }
+    return state
 }
