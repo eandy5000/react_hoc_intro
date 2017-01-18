9dfc950789eaab3e0af55fccf858d78febfa5fa3
Andy Eccleston <eandy5000@yahoo.com>
2017-01-18 15:59:32 -0600

    reset app for rebuild

--- src/components/resources.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default () => {
-    return (
-        <div>
-            <h1>Top secret</h1>
-        </div>
-    )
-}
--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { Router, Route, browserHistory } from 'react-router'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
 
-import App from './components/app';
-import Resources from './components/resources'
-import requireAuth from './components/require_authentication'
-import reducers from './reducers';
+import App from './components/app'
+import reducer from './reducers'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware()(createStore)
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={browserHistory}>
-      <Route path="/" component={App} >
-        <Route path="resources" component={requireAuth(Resources)} />
-      </Route>
-    </Router>
-  </Provider>
-  , document.querySelector('.container'));
+    <Provider store={createStoreWithMiddleware(reducer)} >
+        <App />
+    </Provider>
+    , document.querySelector('.container'))
+
