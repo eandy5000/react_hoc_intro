31245d6de0a4466cbb46968a8883961d0c543515
Andy Eccleston <eandy5000@yahoo.com>
2017-01-19 10:33:27 -0600

    basic routes and components are workging

--- src/index.js
+++ src/index.js
@@ -2,15 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 
 import App from './components/app'
 import reducer from './reducers'
+import Home from './components/home'
+import Resources from './components/resources'
 
 const createStoreWithMiddleware = applyMiddleware()(createStore)
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducer)} >
-        <App />
+        <Router history={browserHistory} >
+            <Route path="/" component={App} >
+                <IndexRoute component={Home} />
+                <Route path="resources" component={Resources} />
+            </Route>
+            
+        </Router>
     </Provider>
     , document.querySelector('.container'))
 
